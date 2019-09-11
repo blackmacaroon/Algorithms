2899da6b51df391767fa8bcde510ba1e3be7c644
Kayla <6lackmacaroon@gmail.com>
2019-09-11 15:46:40 -0600

    understanding the problem and filling in pseudocode

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python
-
+#receives a list of stock prices
+#returns the max profitt from a single day
+#must buy before selling
 import argparse
 
 def find_max_profit(prices):
-  pass
+    #the first number in the list, must BUY before SELLing
+    current_min_price_so_far = prices[0]
+    max_profit_so_far = prices[1] - current_min_price_so_far
+    #loop through the array
+    #enumerate 
+    for i, price in enumerate(prices):
+        print(i, price)
 
+    return max_profit_so_far
+
+find_max_profit([1050, 270, 1540, 3800, 2])
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
@@ -13,3 +24,4 @@ if __name__ == '__main__':
   args = parser.parse_args()
 
   print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+
