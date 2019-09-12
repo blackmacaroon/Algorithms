9cfe527247ef62e18d6a760f2bc145ca087838f7
Kayla <6lackmacaroon@gmail.com>
2019-09-12 16:53:40 -0600

    found a fibonacci video, debugging type error 'int' object had no attribute __getitem__

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,16 +5,26 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
+#look at the ways to eat n-1 cookies, then eat 1 more
+# look at the ways to eat n-2 cookies, then eat 2 more
+#  look at the ways to eat n-3 cookies, then eat 3 more
+e_l_fudge = 8
+cache = {0: 0, 1: 1, 2: 2}
+
 def eating_cookies(n, cache=None):
+  # fibonacci
+  if cache is not None and n in cache:
+    return cache[n]
   if n == 0:
     return 1
-  elif n == 1:
-    return 1
-  elif n = 2:
-    return 2
-  elif n > 2:
+  elif n < 3:
+    return n
+  else: 
+    value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    
+    return value[n]
 
-  pass
+print("mmmmm", eating_cookies(e_l_fudge))
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
