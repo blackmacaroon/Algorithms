809204872bf7d9d87516e5351bfd342cb887b1db
Kayla <6lackmacaroon@gmail.com>
2019-09-12 13:45:25 -0600

    i think recipe_batches works, but not for batches larger than 1. so it answers 'do i have enough for a batch'

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 #recipes arrive in the form of a dictionary
 #ingredients arrive in the form of a dictionary
+#both have same form 
+# need to return the number of batches that can be made with the supplied ingredienst
 import math
 
 def recipe_batches(recipe, ingredients):
+  batches = None
+  #do we have all the ingredients?
+  if len(recipe) > len(ingredients):
+      return 0
+  #if recipe amount > ingredient amount, return 0
+  #loop through both arrays
+  for amount in recipe and ingredients:
+      #if any ingredient in recipe amount is larger than in ingredients, return 0
+      if batches == None or ingredients[amount] // recipe[amount] < batches:
+        batches = ingredients[amount] // recipe[amount]
+      else:
+        return 0
+      
+  return batches
   
 
 
