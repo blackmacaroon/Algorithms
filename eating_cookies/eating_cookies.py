72de07700c10ff36b42870dad509096d2b57dcf9
Kayla <6lackmacaroon@gmail.com>
2019-09-12 20:33:42 -0600

    i'm printing the right number but the tests seem to get stuck in a loop

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -8,23 +8,39 @@ import sys
 #look at the ways to eat n-1 cookies, then eat 1 more
 # look at the ways to eat n-2 cookies, then eat 2 more
 #  look at the ways to eat n-3 cookies, then eat 3 more
-e_l_fudge = 8
-cache = {0: 0, 1: 1, 2: 2}
+e_l_fudge = 3
 
 def eating_cookies(n, cache=None):
   # fibonacci
-  if cache is not None and n in cache:
-    return cache[n]
   if n == 0:
     return 1
   elif n < 3:
     return n
-  else: 
-    value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
-    
-    return value[n]
+  elif cache is not None and cache[n] >= 3:
+    return cache[n]
+  else:
+    temp = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if cache is not None: 
+      cache[n] = temp
+    return temp
 
 print("mmmmm", eating_cookies(e_l_fudge))
+  # if n <= 0:
+  #       return 1
+
+  # if n < 3:
+  #     return n
+
+  # if cache and cache[n] > 0:
+  #     return cache[n]
+
+  # if not cache:
+  #     cache = {i: 0 for i in range(n+1)}
+
+  # cache[n] = eating_cookies(
+  #     n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+  # return cache[n]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
